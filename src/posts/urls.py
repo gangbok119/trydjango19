e4449fae5d9de5ae75cdae562236efb278309682
Kangbok Lee <gangbok119@gmail.com>
2018-01-04 13:11:33 +0900

    template 설정까지 하고 render 테스트(url 매핑 다시 해야함)

---
+++ src/posts/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path
+
+
+from .views import post_home, post_delete, post_update, post_list, post_detail, post_create
+
+urlpatterns = [
+    url(r'', post_home,),
+    url(r'create/$', post_create,),
+    url(r'detail/$', post_detail,),
+    url(r'list/$', post_list,),
+    url(r'update/$', post_update,),
+    url(r'delete/$', post_delete,),
+]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# Create(POST)/Retrieve List Search (GET)/Update(PUT/PATCH)/Delete(DELETE)
+
+
+
+def post_home(request):
+
+    return HttpResponse("<h1>Hello</h1>")
+
+def post_create(request):
+
+    return HttpResponse("<h1>Create</h1>")
+
+
+def post_detail(request):
+
+    return HttpResponse("<h1>Detail</h1>")
+def post_list(request):
+
+    return render(request,"index.html",{})
+def post_update(request):
+
+    return HttpResponse("<h1>Update</h1>")
+def post_delete(request):
+
+    return HttpResponse("<h1>Delete</h1>")
+
