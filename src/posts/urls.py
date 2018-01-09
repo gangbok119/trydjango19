f664d8a8f84fe4e60155da73d5aa03ca7b856416
Kangbok Lee <gangbok119@gmail.com>
2018-01-09 15:21:18 +0900

    18. dynamic url routing

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 from .views import post_home, post_delete, post_update, post_list, post_detail, post_create
 
 urlpatterns = [
-    url(r'', post_home,),
-    url(r'create/$', post_create,),
-    url(r'detail/$', post_detail,),
-    url(r'list/$', post_list,),
-    url(r'update/$', post_update,),
-    url(r'delete/$', post_delete,),
+    url(r'^$', post_home,),
+    url(r'^create/$', post_create,),
+    url(r'^detail/(?P<pk>\d+)/$', post_detail,),
+    url(r'^list/$', post_list,),
+    url(r'^update/$', post_update,),
+    url(r'^delete/$', post_delete,),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,30 +1,50 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 # Create(POST)/Retrieve List Search (GET)/Update(PUT/PATCH)/Delete(DELETE)
-
+from .models import Post
 
 
 def post_home(request):
-
     return HttpResponse("<h1>Hello</h1>")
 
-def post_create(request):
 
+def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def post_detail(request):
+def post_detail(request,pk=None):  # retrieve
+    instance = get_object_or_404(Post,pk=pk)
 
-    return HttpResponse("<h1>Detail</h1>")
-def post_list(request):
+    context = {
+        "title": instance.title,
+        "instance":instance
+    }
 
-    return render(request,"index.html",{})
+    return render(request, "post_detail.html", context=context)
+
+
+def post_list(request):  # List items
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User List"
+    #     }
+    #     return render(request, "index.html", context=context)
+    #
+    # else:
+
+    queryset = Post.objects.all()
+    context = {
+        "object_list":queryset,
+        "title": "List"
+    }
+    return render(request, "index.html", context=context)
+
+
 def post_update(request):
-
     return HttpResponse("<h1>Update</h1>")
+
+
 def post_delete(request):
-
     return HttpResponse("<h1>Delete</h1>")
-
